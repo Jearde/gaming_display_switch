cefd968991d828af2e0364de6884cf2b0f6ba28e
Rene Glitza <rene.glitza@ruhr-uni-bochum.de>
2023-12-17 21:37:18 +0100

    Fix: scripts not found

--- main.ps1
+++ main.ps1
@@ -1,10 +1,15 @@
 Set-Location $PSScriptRoot
+Push-Location $PSScriptRoot
+
+# Write-Host $PSScriptRoot
+
+Get-Location
 
 Write-Host 'Switching TV state'
-toggle_screen.ps1
+.\toggle_screen.ps1
 Write-Host 'Waiting for 5 s'
 Start-Sleep -Seconds 5
 Write-Host 'Close Steam and start Steam Beta in Gamepad mode'
-start_steam_beta.ps1
+.\start_steam_beta.ps1
 
 Exit
